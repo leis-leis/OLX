4014595c24928c4683bce447b1883e37178ebbb8
leis-leis <krzysztofwajdzik1207@gmail.com>
2023-03-12 18:50:25 +0100

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const port = 3000
 app.use(express.json())
 
 
+mongoose.connect('mongodb://localhost:27017/OLX',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+
 app.get('/', async (req, res) => {
     console.log('hit')
     
